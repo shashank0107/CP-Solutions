7f71b30bcec73f1669e873a0b57821cb17e58f20
shashank0107 <35562709+shashank0107@users.noreply.github.com>
2018-12-31 11:43:45 +0530

    Create UVA 562 Dividing Coins.cpp

---
+++ UVA 562 Dividing Coins.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+
+/*** Template Begins ***/
+
+typedef long long    ll;
+typedef pair<ll,ll>  PII;
+typedef pair<ll, pair<int, int> > PIII;
+typedef vector<int>  vi;
+
+#define endl         '\n'
+#define pb           push_back
+#define INF          INT_MAX/10
+#define F            first
+#define S            second
+#define mp           make_pair
+#define ios          ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define hell         1000000007
+#define all(a)       (a).begin(),(a).end()
+#define M_PI         3.14159265358979323846264338327950288
+ll power(ll x, ll y, ll p){ ll res = 1;x = x % p;while (y > 0){ if (y & 1) res = (res*x) % p;y = y>>1;x = (x*x) % p;} return res; } 
+
+// Debug //
+
+#define trace(x)                 cerr << #x << ": " << x << endl;
+#define trace1(x)                cerr << #x << ": " << x << endl;
+#define trace2(x, y)             cerr << #x << ": " << x << " | " << #y << ": " << y << endl;
+#define trace3(x, y, z)          cerr << #x << ": " << x << " | " << #y << ": " << y << " | " << #z << ": " << z << endl;
+#define trace4(a, b, c, d)       cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << endl;
+#define trace5(a, b, c, d, e)    cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << endl;
+#define trace6(a, b, c, d, e, f) cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << " | " << #f << ": " << f << endl;
+
+// Constants //
+
+const int N = 1e5+7;
+const int xinc[] = {0, 0, 1, -1};
+const int yinc[] = {1, -1, 0, 0};
+
+/*** Template Ends ***/
+
+int dp[105][N], n, a[105], total;
+
+void pre(){
+	total = 0;
+	memset(dp, -1, sizeof(dp));
+}
+
+int f(int idx, int cur_sum){
+
+	if (idx == n)	return abs(cur_sum-(total-cur_sum));
+
+	if (dp[idx][cur_sum] != -1)	return dp[idx][cur_sum];
+
+	dp[idx][cur_sum] = min(f(idx+1, cur_sum+a[idx]), f(idx+1, cur_sum));
+	return dp[idx][cur_sum];
+}
+
+void solve(){
+	
+	pre();
+	cin >> n;
+	for(int i = 0; i < n; i++){	cin >> a[i];	total+=a[i];	}
+
+	cout << f(0, 0) << endl;
+
+}
+
+int main(){
+	
+	ios;
+	// freopen("input.txt", "r", stdin);
+	// freopen("output.txt", "w", stdout);
+	int t;
+	cin >> t;
+	//t = 1;
+	while(t--)
+		solve();
+
+	return 0;
+}
