22f4171abb935bfb4d0de37de0c4f2d24f7d344c
shashank0107 <35562709+shashank0107@users.noreply.github.com>
2019-01-05 16:53:28 +0530

    Create UVA 10617.cpp

---
+++ UVA 10617.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+
+/*** Template Begins ***/
+
+typedef long long    ll;
+typedef pair<ll,ll>  PII;
+typedef pair<ll, pair<int, int> > PIII;
+typedef vector<int>  vi;
+
+#define endl         '\n'
+#define pb           push_back
+#define INF          INT_MAX/10
+#define F            first
+#define S            second
+#define mp           make_pair
+#define ios          ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define hell         1000000007
+#define all(a)       (a).begin(),(a).end()
+#define M_PI         3.14159265358979323846264338327950288
+ll power(ll x, ll y, ll p){ ll res = 1;x = x % p;while (y > 0){ if (y & 1) res = (res*x) % p;y = y>>1;x = (x*x) % p;} return res; } 
+
+// Debug //
+
+#define trace(x)                 cerr << #x << ": " << x << endl;
+#define trace1(x)                cerr << #x << ": " << x << endl;
+#define trace2(x, y)             cerr << #x << ": " << x << " | " << #y << ": " << y << endl;
+#define trace3(x, y, z)          cerr << #x << ": " << x << " | " << #y << ": " << y << " | " << #z << ": " << z << endl;
+#define trace4(a, b, c, d)       cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << endl;
+#define trace5(a, b, c, d, e)    cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << endl;
+#define trace6(a, b, c, d, e, f) cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << " | " << #f << ": " << f << endl;
+
+// Constants //
+
+const int N = 60+7;
+const int xinc[] = {0, 0, 1, -1};
+const int yinc[] = {1, -1, 0, 0};
+
+/*** Template Ends ***/
+string s;
+int n;
+ll dp[N][N];
+
+ll count(int i, int j){
+
+	if (i == j)	return 1;
+	if (i > j)	return 0;
+
+	if (dp[i][j] != -1)	return dp[i][j];
+
+	ll &ret = dp[i][j];
+
+	if (s[i]==s[j])	ret = count(i+1, j) + count(i, j-1) + 1;
+	else ret = count(i+1, j) + count(i, j-1) - count(i+1, j-1);
+
+	return ret;
+}
+
+
+void solve(){
+	
+	memset(dp, -1, sizeof dp);
+	cin >> s;
+	n = s.size();
+	ll ans = count(0, n-1);
+	cout << ans << endl;
+}
+
+int main(){
+	
+	ios;
+	// freopen("input.txt", "r", stdin);
+	// freopen("output.txt", "w", stdout);
+	int t;
+	cin >> t;
+	//t = 1;
+	while(t--)
+		solve();
+
+	return 0;
+}
