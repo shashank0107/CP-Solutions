b1aefc396ba7073680059946c12e469e8191ba23
shashank0107 <35562709+shashank0107@users.noreply.github.com>
2019-01-13 15:14:14 +0530

    Create UVA 10790 How many pts of intersection?.cpp

---
+++ UVA 10790 How many pts of intersection?.cpp
@@ -0,0 +1,96 @@
+#include <bits/stdc++.h>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+
+/*** Template Begins ***/
+
+typedef long long    ll;
+typedef pair<ll,ll>  PII;
+typedef pair<ll, pair<int, int> > PIII;
+typedef vector<int>  vi;
+
+#define endl         '\n'
+#define pb           push_back
+#define INF          INT_MAX/10
+#define F            first
+#define S            second
+#define mp           make_pair
+#define ios          ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define hell         1000000007
+#define all(a)       (a).begin(),(a).end()
+//#define M_PI         3.14159265358979323846264338327950288
+ll power(ll x, ll y, ll p){ ll res = 1;x = x % p;while (y > 0){ if (y & 1) res = (res*x) % p;y = y>>1;x = (x*x) % p;} return res; } 
+
+// Debug //
+
+#define trace(x)                 cerr << #x << ": " << x << endl;
+#define trace1(x)                cerr << #x << ": " << x << endl;
+#define trace2(x, y)             cerr << #x << ": " << x << " | " << #y << ": " << y << endl;
+#define trace3(x, y, z)          cerr << #x << ": " << x << " | " << #y << ": " << y << " | " << #z << ": " << z << endl;
+#define trace4(a, b, c, d)       cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << endl;
+#define trace5(a, b, c, d, e)    cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << endl;
+#define trace6(a, b, c, d, e, f) cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << " | " << #f << ": " << f << endl;
+
+// Constants //
+
+const int N = 2e4+7;
+const int xinc[] = {0, 0, 1, -1};
+const int yinc[] = {1, -1, 0, 0};
+
+/*** Template Ends ***/
+
+ll dp[N], dp2[N], n, a, b;
+
+ll calc(ll n, ll a){
+	return ((n*(n+1))/2)*a;
+}
+
+void pre(){
+
+	dp[1] = 0;
+	dp2[1] = 0;
+	for(int i = 2; i <= 2e4; i++)
+	{
+		dp[i] = dp2[i-1] + calc(i-1, i-1);
+		dp2[i] = dp[i] + calc(i-1, i);
+	}
+}
+
+ll f(ll a, ll b){
+
+	if (a > b)	swap(a, b);
+	if (a == 1)	return 0;
+
+	if (a == b)	return dp[a];
+
+	return f(a, b-1) + calc(a-1, b-1);
+
+}
+
+void solve(){
+	
+	pre();
+	int c = 1;
+	while(cin >> a >> b){
+		if (a==0)	break;
+
+		cout << "Case " << c++ << ": " << f(a, b) << endl;
+	}
+
+
+}
+
+int main(){
+	
+	ios;
+	// freopen("input.txt", "r", stdin);
+	// freopen("output.txt", "w", stdout);
+	int t;
+	//cin >> t;
+	t = 1;
+	while(t--)
+		solve();
+
+	return 0;
+}
