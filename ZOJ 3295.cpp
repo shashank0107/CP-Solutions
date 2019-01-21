842cadc6c0472ad21690361267a61dad8f75236d
shashank0107 <35562709+shashank0107@users.noreply.github.com>
2019-01-21 13:50:24 +0530

    Create ZOJ 3295.cpp

---
+++ ZOJ 3295.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+
+/*** Template Begins ***/
+
+typedef long long    ll;
+typedef pair<ll,ll>  PII;
+typedef pair<ll, pair<int, int> > PIII;
+typedef vector<int>  vi;
+
+#define endl         '\n'
+#define pb           push_back
+#define INF          INT_MAX/10
+#define F            first
+#define S            second
+#define mp           make_pair
+#define ios          ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define hell         1000000007
+#define all(a)       (a).begin(),(a).end()
+//#define M_PI         3.14159265358979323846264338327950288
+ll power(ll x, ll y, ll p){ ll res = 1;x = x % p;while (y > 0){ if (y & 1) res = (res*x) % p;y = y>>1;x = (x*x) % p;} return res; } 
+
+// Debug //
+
+#define trace(x)                 cerr << #x << ": " << x << endl;
+#define trace1(x)                cerr << #x << ": " << x << endl;
+#define trace2(x, y)             cerr << #x << ": " << x << " | " << #y << ": " << y << endl;
+#define trace3(x, y, z)          cerr << #x << ": " << x << " | " << #y << ": " << y << " | " << #z << ": " << z << endl;
+#define trace4(a, b, c, d)       cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << endl;
+#define trace5(a, b, c, d, e)    cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << endl;
+#define trace6(a, b, c, d, e, f) cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << " | " << #f << ": " << f << endl;
+
+// Constants //
+
+const int N = (1<<10)+7;
+const int xinc[] = {0, 0, 1, -1};
+const int yinc[] = {1, -1, 0, 0};
+
+/*** Template Ends ***/
+int a[N], n, k, val;
+
+
+void solve(){
+	
+	while(cin >> n >> k){
+
+		for(int i = 0; i < (1<<n); i++){	
+			cin >> a[i];
+			if (i == k)	val = a[i];	
+		}
+		// find the no. of elements lower than a[i];
+		int cnt = 0;
+		for(int i = 0; i < (1<<n); i++)	if (a[i] < val)	cnt++;
+
+		if (cnt == (1<<n)-1){	cout << n << " " << n << endl;	continue;	}
+
+		int mx = log2(cnt+1);
+		int mn = 0;
+		trace4(val, mx, mn, cnt);
+
+		cout << mn << " " << mx << endl;
+	}
+}
+
+int main(){
+	
+	ios;
+	// freopen("input.txt", "r", stdin);
+	// freopen("output.txt", "w", stdout);
+	int t;
+	//cin >> t;
+	t = 1;
+	while(t--)
+		solve();
+
+	return 0;
+}
