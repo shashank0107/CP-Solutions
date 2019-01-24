f5077c338adc57b3fbfe5c84b19ed755b0ebc020
shashank0107 <35562709+shashank0107@users.noreply.github.com>
2019-01-24 14:51:32 +0530

    Create UVA 737.cpp

---
+++ UVA 737.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+
+/*** Template Begins ***/
+
+typedef long long    ll;
+typedef pair<ll,ll>  PII;
+typedef pair<ll, pair<int, int> > PIII;
+typedef vector<int>  vi;
+
+#define endl         '\n'
+#define pb           push_back
+#define INF          INT_MAX/10
+#define F            first
+#define S            second
+#define mp           make_pair
+#define ios          ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define hell         1000000007
+#define all(a)       (a).begin(),(a).end()
+ll power(ll x, ll y, ll p){ ll res = 1;x = x % p;while (y > 0){ if (y & 1) res = (res*x) % p;y = y>>1;x = (x*x) % p;} return res; } 
+
+// Debug //
+
+#define trace(x)                 cerr << #x << ": " << x << endl;
+#define trace1(x)                cerr << #x << ": " << x << endl;
+#define trace2(x, y)             cerr << #x << ": " << x << " | " << #y << ": " << y << endl;
+#define trace3(x, y, z)          cerr << #x << ": " << x << " | " << #y << ": " << y << " | " << #z << ": " << z << endl;
+#define trace4(a, b, c, d)       cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << endl;
+#define trace5(a, b, c, d, e)    cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << endl;
+#define trace6(a, b, c, d, e, f) cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << " | " << #f << ": " << f << endl;
+
+// Constants //
+
+const int N = 1e5+7;
+const int xinc[] = {0, 0, 1, -1};
+const int yinc[] = {1, -1, 0, 0};
+const long double PI = acos(-1.0);
+const double EPS = 1e-9;
+
+/*** Template Ends ***/
+
+int n;
+
+void solve(){
+    
+    while(cin >> n)
+    {
+        if (n == 0) break;
+
+        vector<int> sx(n), sy(n), sz(n), ex(n), ey(n), ez(n);
+        int t;
+
+        for(int i = 0; i < n; i++){  
+            cin >> sx[i] >> sy[i] >> sz[i] >> t;
+            ex[i] = sx[i]+t;    
+            ey[i] = sy[i]+t;
+            ez[i] = sz[i]+t; 
+        }
+        sort(all(sx));
+        sort(all(sy));
+        sort(all(sz));
+        sort(all(ex));
+        sort(all(ey));
+        sort(all(ez));
+
+        int tx = sx.back(), ty = sy.back(), tz = sz.back();
+        int dx = ex[0], dy = ey[0], dz = ez[0];
+
+        if (tx < dx && ty < dy && tz < dz)
+            cout << (dz-tz)*(dy-ty)*(dx-tx) << endl;
+        else cout << 0 << endl;
+    }
+}
+
+int main(){
+    
+    ios;
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    int t;
+    //cin >> t;
+    t = 1;
+    while(t--)
+        solve();
+
+    return 0;
+}
