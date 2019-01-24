8d285626e3e5a9ec252eb1eb8171974b4de883f9
shashank0107 <35562709+shashank0107@users.noreply.github.com>
2019-01-24 21:23:04 +0530

    Create UVA 356.cpp

---
+++ UVA 356.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+
+/*** Template Begins ***/
+
+typedef long long    ll;
+typedef pair<ll,ll>  PII;
+typedef pair<ll, pair<int, int> > PIII;
+typedef vector<int>  vi;
+
+#define endl         '\n'
+#define pb           push_back
+#define INF          INT_MAX/10
+#define F            first
+#define S            second
+#define mp           make_pair
+#define ios          ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define hell         1000000007
+#define all(a)       (a).begin(),(a).end()
+ll power(ll x, ll y, ll p){ ll res = 1;x = x % p;while (y > 0){ if (y & 1) res = (res*x) % p;y = y>>1;x = (x*x) % p;} return res; } 
+
+// Debug //
+
+#define trace(x)                 cerr << #x << ": " << x << endl;
+#define trace1(x)                cerr << #x << ": " << x << endl;
+#define trace2(x, y)             cerr << #x << ": " << x << " | " << #y << ": " << y << endl;
+#define trace3(x, y, z)          cerr << #x << ": " << x << " | " << #y << ": " << y << " | " << #z << ": " << z << endl;
+#define trace4(a, b, c, d)       cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << endl;
+#define trace5(a, b, c, d, e)    cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << endl;
+#define trace6(a, b, c, d, e, f) cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << " | " << #f << ": " << f << endl;
+
+// Constants //
+
+const int N = 1e5+7;
+const int xinc[] = {0, 0, 1, -1};
+const int yinc[] = {1, -1, 0, 0};
+const long double PI = acos(-1.0);
+const double EPS = 1e-9;
+
+/*** Template Ends ***/
+
+double dist(double x, double y){
+    return (x*x+y*y);
+}
+int n;
+void solve(){
+    
+    while(cin >> n)
+    {
+        double r = (2*(double)n-1)/2;
+        int in = 0, on = 0;
+        for(int i = 1; i <= n; i++)
+            for(int j = 1; j <= n; j++)
+            {
+                if (dist(i-1, j-1) < r*r + EPS && dist(i, j) < r*r + EPS)   in++;
+                else if (dist(i-1, j-1) <= r*r + EPS && dist(i, j) >= r*r + EPS)    on++;
+                //trace5(i, j, dist(i-1, j-1), dist(i, j), r*r);
+            }
+
+        cout << "In the case n = " << n << ", " << 4*on << " cells contain segments of the circle." << endl;
+        cout << "There are " << 4*in << " cells completely contained in the circle." << endl;
+        cout << endl;
+
+    }
+
+
+}
+
+int main(){
+    
+    ios;
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    int t;
+    //cin >> t;
+    t = 1;
+    while(t--)
+        solve();
+
+    return 0;
+}
