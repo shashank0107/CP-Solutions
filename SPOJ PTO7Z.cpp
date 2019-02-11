7c2db2491646c38416995614c1bd9dcf7d012642
shashank0107 <35562709+shashank0107@users.noreply.github.com>
2019-02-11 13:47:30 +0530

    Create SPOJ PTO7Z.cpp

---
+++ SPOJ PTO7Z.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+
+/*** Template Begins ***/
+
+typedef long long    ll;
+typedef pair<ll,ll>  PII;
+typedef pair<ll, pair<int, int> > PIII;
+typedef vector<int>  vi;
+
+#define endl         '\n'
+#define pb           push_back
+#define INF          INT_MAX/10
+#define F            first
+#define S            second
+#define mp           make_pair
+#define ios          ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define hell         1000000007
+#define all(a)       (a).begin(),(a).end()
+ll power(ll x, ll y, ll p){ ll res = 1;x = x % p;while (y > 0){ if (y & 1) res = (res*x) % p;y = y>>1;x = (x*x) % p;} return res; } 
+
+// Debug //
+
+#define trace(x)                 cerr << #x << ": " << x << endl;
+#define trace1(x)                cerr << #x << ": " << x << endl;
+#define trace2(x, y)             cerr << #x << ": " << x << " | " << #y << ": " << y << endl;
+#define trace3(x, y, z)          cerr << #x << ": " << x << " | " << #y << ": " << y << " | " << #z << ": " << z << endl;
+#define trace4(a, b, c, d)       cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << endl;
+#define trace5(a, b, c, d, e)    cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << endl;
+#define trace6(a, b, c, d, e, f) cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << " | " << #f << ": " << f << endl;
+
+// Constants //
+
+const int N = 1e4+7;
+const int xinc[] = {0, 0, 1, -1};
+const int yinc[] = {1, -1, 0, 0};
+const long double PI = acos(-1.0);
+const double EPS = 1e-9;
+
+/*** Template Ends ***/
+int n, a, b, ans;
+bool vis[N];
+vi g[N];
+
+int ht(int v){
+
+	vis[v] = true;
+	int h1 = 0, h2 = 0;
+
+	for(auto nb : g[v])
+		if (!vis[nb])
+		{
+			int temp = ht(nb);
+			if (temp > h1){	h2 = h1; h1 = temp;	}
+			else if (temp > h2){	h2 = temp;	}
+		}
+	ans = max(ans, 1+h1+h2);
+	return 1+h1;
+}
+
+
+void solve(){
+	
+	cin >> n;
+	for(int i = 0; i < n-1; i++){
+		cin >> a >> b;
+		g[a].pb(b);
+		g[b].pb(a);
+	}
+	ht(1);
+	cout << ans-1 << endl;
+}
+
+int main(){
+	
+	ios;
+	// freopen("input.txt", "r", stdin);
+	// freopen("output.txt", "w", stdout);
+	int t;
+	//cin >> t;
+	t = 1;
+	while(t--)
+		solve();
+
+	return 0;
+}
