63a87da4d293a069ab7024914e991b03937982ba
shashank0107 <35562709+shashank0107@users.noreply.github.com>
2019-04-10 19:20:11 +0530

    Create kth smallest subarray sum.cpp

---
+++ 2 Pointers/kth smallest subarray sum.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+
+/*** Template Begins ***/
+
+typedef long long                   ll;
+typedef pair<ll,ll>                 PII;
+typedef pair<ll, pair<int, int> >   PIII;
+typedef vector<int>                 vi;
+typedef vector<pair<int, int> >     vii;
+
+#define endl         '\n'
+#define pb           push_back
+#define INF          INT_MAX/10
+#define F            first
+#define S            second
+#define mp           make_pair
+#define ios          ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define hell         1000000007
+#define all(a)       (a).begin(),(a).end()
+#define fr(i,a,b)    for(int i=a;i<=b;i++)
+#define lp(i,a)      for(int i=0;i< a;i++)
+
+// Debug //
+
+#define trace(x)                 cerr << #x << ": " << x << endl;
+#define trace1(x)                cerr << #x << ": " << x << endl;
+#define trace2(x, y)             cerr << #x << ": " << x << " | " << #y << ": " << y << endl;
+#define trace3(x, y, z)          cerr << #x << ": " << x << " | " << #y << ": " << y << " | " << #z << ": " << z << endl;
+#define trace4(a, b, c, d)       cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << endl;
+#define trace5(a, b, c, d, e)    cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << endl;
+#define trace6(a, b, c, d, e, f) cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << " | " << #f << ": " << f << endl;
+
+// Constants //
+
+const int N = 1e5+7;
+const int xinc[] = {0, 0, 1, -1};
+const int yinc[] = {1, -1, 0, 0};
+const long double PI = acos(-1.0);
+const double EPS = 1e-9;
+
+/*** Template Ends ***/
+ll n, a[N], k;
+
+bool f(ll sum){
+    int cnt = 0;
+    ll cur = 0;
+    int j = 0;
+    for(int i = 0; i < n; i++){
+        while(j < n && cur + a[j] <= sum)
+            cur += a[j++];
+        if (j > i) cnt += j-i;
+        cur -= a[i];
+    }
+    return cnt >= k;
+}
+
+void solve(){
+    
+    cin >> n >> k;
+    lp(i,n) cin >> a[i];
+    ll lo = 0, hi = 1e15;
+    //trace(f(3));
+    while(hi-lo>1){
+        ll mid = (hi+lo)/2;
+        if (f(mid)) hi = mid;
+        else lo = mid;
+    }
+    if (f(lo)) cout << lo << endl;
+    else cout << hi << endl;
+}
+
+signed main(){
+    
+    ios;
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    int t;
+    //cin >> t;
+    t = 1;
+    while(t--)
+        solve();
+
+    return 0;
+}
