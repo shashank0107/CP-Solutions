bc8f0821c5b6157b02b29ee7d9396278b5a562b5
shashank0107 <35562709+shashank0107@users.noreply.github.com>
2019-04-18 15:35:55 +0530

    Create UVA 12024.cpp

---
+++ UVA 12024.cpp
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*** Template Begins ***/
+#define int ll
+typedef long long                   ll;
+typedef pair<int,int>               PII;
+typedef pair<ll, pair<int, int> >   PIII;
+typedef vector<int>                 vi;
+typedef vector<pair<int, int> >     vii;
+
+#define endl         '\n'
+#define pb           push_back
+#define INF          INT_MAX/10
+#define F            first
+#define S            second
+#define mp           make_pair
+#define ios          ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define hell         1000000007
+#define all(a)       (a).begin(),(a).end()
+#define fr(i,a,b)    for(int i=a;i<=b;i++)
+#define lp(i,a)      for(int i=0;i< a;i++)
+
+// Debug //
+
+#define trace(x)                 cerr << #x << ": " << x << endl;
+#define trace2(x, y)             cerr << #x << ": " << x << " | " << #y << ": " << y << endl;
+#define trace3(x, y, z)          cerr << #x << ": " << x << " | " << #y << ": " << y << " | " << #z << ": " << z << endl;
+#define trace4(a, b, c, d)       cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << endl;
+#define trace5(a, b, c, d, e)    cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << endl;
+#define trace6(a, b, c, d, e, f) cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << " | " << #f << ": " << f << endl;
+
+// Constants //
+
+const int N = 1e5+7;
+const int xinc[] = {0, 0, 1, -1};
+const int yinc[] = {1, -1, 0, 0};
+const long double PI = acos(-1.0);
+
+/*** Template Ends ***/
+
+int n, dp[13][1<<13];
+
+
+int calc(int i, int mask){
+
+    //trace2(i,mask);
+    if (i == n)    return 1;
+    int &ret = dp[i][mask];
+    if (ret != -1)  return ret;
+    ret = 0;
+
+    for(int j = 0; j < n; j++)
+    {
+        if ((mask & (1<<j)) == 0 && j != i)
+            ret += calc(i+1, mask|(1<<j));
+    }
+    //trace3(i,mask,ret);
+    return ret;
+}
+int fac[15];
+
+void solve(){
+    
+    cin >> n;
+    memset(dp, -1, sizeof dp);
+
+    cout << calc(0, 0) << "/" << fac[n] << endl;
+}
+
+signed main(){
+    
+    fac[0] = 1;
+    fr(i,1,13)  fac[i] = i*fac[i-1];
+    ios;
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    int t;
+    cin >> t;
+    //t = 1;
+    while(t--)
+        solve();
+
+    return 0;
+}
