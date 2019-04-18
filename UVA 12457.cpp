cce897b79a7e66ace59ddc13378b7a2186849cfa
shashank0107 <35562709+shashank0107@users.noreply.github.com>
2019-04-18 16:13:41 +0530

    Create UVA 12457.cpp

---
+++ UVA 12457.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*** Template Begins ***/
+
+typedef long long                   ll;
+typedef pair<int,int>               PII;
+typedef pair<ll, pair<int, int> >   PIII;
+typedef vector<int>                 vi;
+typedef vector<pair<int, int> >     vii;
+
+#define endl         '\n'
+#define pb           push_back
+#define INF          INT_MAX/10
+#define F            first
+#define S            second
+#define mp           make_pair
+#define ios          ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define hell         1000000007
+#define all(a)       (a).begin(),(a).end()
+#define fr(i,a,b)    for(int i=a;i<=b;i++)
+#define lp(i,a)      for(int i=0;i< a;i++)
+
+// Debug //
+
+#define trace(x)                 cerr << #x << ": " << x << endl;
+#define trace2(x, y)             cerr << #x << ": " << x << " | " << #y << ": " << y << endl;
+#define trace3(x, y, z)          cerr << #x << ": " << x << " | " << #y << ": " << y << " | " << #z << ": " << z << endl;
+#define trace4(a, b, c, d)       cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << endl;
+#define trace5(a, b, c, d, e)    cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << endl;
+#define trace6(a, b, c, d, e, f) cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << " | " << #f << ": " << f << endl;
+
+// Constants //
+
+const int N = 1e5+7;
+const int xinc[] = {0, 0, 1, -1};
+const int yinc[] = {1, -1, 0, 0};
+const long double PI = acos(-1.0);
+
+/*** Template Ends ***/
+int n;
+double p;
+
+double dp[60][60];
+
+double calc(int i, int need){
+
+    int remain = 2*n-1-i;
+    if (remain < need)  return 0;
+    if (need == 0)  return 1;
+
+
+    double &ret = dp[i][need];
+    if (ret != -1.0)    return  ret;
+    ret = 0;
+
+    ret += p*calc(i+1, need-1);
+    ret += (1-p)*calc(i+1, need);
+    return ret;
+}
+
+void solve(){
+
+    cin >> n >> p;
+    lp(i,60) lp(j,60) dp[i][j] = -1;
+
+    cout << calc(0,n) << endl;
+}
+
+signed main(){
+    
+    ios;
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    cout << fixed << setprecision(2);
+    int t;
+    cin >> t;
+    //t = 1;
+    while(t--)
+        solve();
+
+    return 0;
+}
