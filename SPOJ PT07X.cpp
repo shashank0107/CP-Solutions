2171738ee3c3142b6ad1f6f1ac054ace92a21696
shashank0107 <35562709+shashank0107@users.noreply.github.com>
2019-05-29 22:37:00 +0530

    Create SPOJ PT07X.cpp

---
+++ SPOJ PT07X.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*** Template Begins ***/
+
+typedef long long                   ll;
+typedef pair<int,int>               PII;
+typedef pair<ll, pair<int, int> >   PIII;
+typedef vector<int>                 vi;
+typedef vector<pair<int, int> >     vii;
+
+#define endl         '\n'
+#define pb           push_back
+#define INF          INT_MAX/10
+#define F            first
+#define S            second
+#define mp           make_pair
+#define ios          ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define hell         1000000007
+#define all(a)       (a).begin(),(a).end()
+#define fr(i,a,b)    for(int i=a;i<=b;i++)
+#define lp(i,a)      for(int i=0;i< a;i++)
+
+// Debug //
+
+#define trace(x)                 cerr << #x << ": " << x << endl;
+#define trace2(x, y)             cerr << #x << ": " << x << " | " << #y << ": " << y << endl;
+#define trace3(x, y, z)          cerr << #x << ": " << x << " | " << #y << ": " << y << " | " << #z << ": " << z << endl;
+#define trace4(a, b, c, d)       cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << endl;
+#define trace5(a, b, c, d, e)    cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << endl;
+#define trace6(a, b, c, d, e, f) cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << " | " << #f << ": " << f << endl;
+
+// Constants //
+
+const int N = 1e5+7;
+const int xinc[] = {0, 0, 1, -1};
+const int yinc[] = {1, -1, 0, 0};
+const long double PI = acos(-1.0);
+
+/*** Template Ends ***/
+int n, a, b;
+vi g[N];
+bool vis[N];
+int dp[N][2];
+
+void dfs(int v) {
+
+    vis[v] = true;
+    dp[v][1] = 1;
+    dp[v][0] = 0;
+
+    for (auto nb : g[v])
+        if (!vis[nb]) {
+            dfs(nb);
+            dp[v][0] += dp[nb][1];
+            dp[v][1] += min(dp[nb][1], dp[nb][0]);
+        }
+}
+
+void solve() {
+
+    cin >> n;
+    lp(i,n-1) {
+        cin >> a >> b;
+        g[a].pb(b);
+        g[b].pb(a);
+    }
+    dfs(1);
+    cout << min(dp[1][0], dp[1][1]) << endl;
+}
+
+signed main() {
+
+    ios;
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    int t;
+    //cin >> t;
+    t = 1;
+    while(t--)
+        solve();
+
+    return 0;
+}
