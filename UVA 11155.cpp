e9933a8611bdb3581d59d12136c88c319331118b
shashank0107 <35562709+shashank0107@users.noreply.github.com>
2019-05-30 23:22:31 +0530

    Create UVA 11155.cpp

---
+++ UVA 11155.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*** Template Begins ***/
+#define int ll
+typedef long long                   ll;
+typedef pair<int,int>               PII;
+typedef pair<ll, pair<int, int> >   PIII;
+typedef vector<int>                 vi;
+typedef vector<pair<int, int> >     vii;
+
+#define endl         '\n'
+#define pb           push_back
+#define INF          INT_MAX/10
+#define F            first
+#define S            second
+#define mp           make_pair
+#define ios          ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define hell         1000000007
+#define all(a)       (a).begin(),(a).end()
+#define fr(i,a,b)    for(int i=a;i<=b;i++)
+#define lp(i,a)      for(int i=0;i< a;i++)
+
+// Debug //
+
+#define trace(x)                 cerr << #x << ": " << x << endl;
+#define trace2(x, y)             cerr << #x << ": " << x << " | " << #y << ": " << y << endl;
+#define trace3(x, y, z)          cerr << #x << ": " << x << " | " << #y << ": " << y << " | " << #z << ": " << z << endl;
+#define trace4(a, b, c, d)       cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << endl;
+#define trace5(a, b, c, d, e)    cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << endl;
+#define trace6(a, b, c, d, e, f) cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << " | " << #f << ": " << f << endl;
+
+// Constants //
+
+const int N = 1e4+7;
+const int xinc[] = {0, 0, 1, -1};
+const int yinc[] = {1, -1, 0, 0};
+const long double PI = acos(-1.0);
+
+/*** Template Ends ***/
+int a, b, c, m, n, last;
+int v[N];
+
+void solve(int tc) {
+
+    cin >> a >> b >> c >> m >> n;
+    ll ans = 0;
+
+    v[0] = a;
+    fr(i,1,n-1) {
+        v[i] = (v[i-1]*b + c)%m + 1;
+    }
+
+    vector<int> cnt(m+4, 0);
+    cnt[0] = 1;
+
+    int cur_sum = 0;
+
+    lp(i,n) {
+        cur_sum = (cur_sum + v[i])%m;
+        ans += cnt[cur_sum];
+        cnt[cur_sum]++;
+    }
+    cout << "Case " << tc << ": " << ans << endl;
+}
+
+signed main() {
+
+    ios;
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    int t;
+    cin >> t;
+    //t = 1;
+    fr(i,1,t)
+        solve(i);
+
+    return 0;
+}
